8b61543d73561606706e55374a49d099c72d1bf4
Ivan Rodrigo <191221@ids.upchiapas.edu.mx>
2021-10-22 00:14:39 -0500

    Archivo_Terminado

---
+++ actividad.py
@@ -0,0 +1,48 @@
+import threading
+import time
+import queue
+from threading import Thread, Lock
+mutex = Lock()
+
+queue = queue.Queue(maxsize=5)
+
+class TenedorFilosofo(threading.Thread):
+    def __init__(self, tenedores, filosofosNum):
+        threading.Thread.__init__(self)
+        self.tenedores = tenedores
+        self.filosofosNum = filosofosNum
+        
+   
+    def iniciar(self):
+        mutex.acquire()
+        print("Filosofo iniciando", self.filosofosNum)
+        time.sleep(2)
+        print("Filosofo ", self.filosofosNum, "recoge tenedor del lado izquierdo")
+        self.tenedores[self.filosofosNum]
+        time.sleep(2)
+        print("Filosofo ", self.filosofosNum, "recoge tenedor del lado derecho")
+        self.tenedores[self.filosofosNum]
+        time.sleep(2)
+    
+    def terminar(self):
+      
+        print("Filosofo ", self.filosofosNum, "libre izquierdo")
+        self.tenedores[self.filosofosNum]
+        time.sleep(2)
+        print("Filosofo ", self.filosofosNum, "libre derecho")
+        self.tenedores[self.filosofosNum]
+        time.sleep(2)
+        mutex.release()
+
+    def run(self):
+        self.iniciar()
+        self.terminar()
+
+tenedorArray = [1,1,1,1,1]
+
+for i in range(0,5):
+    tenedorArray[i] = threading.BoundedSemaphore(1)
+
+for i in range(0,5):
+    total = TenedorFilosofo(tenedorArray, i)
+    total.start()
